da64180987f5f8708fcff8224eec2ec4abebd28d
Varneon <me@varneon.com>
2023-01-23 20:31:55 -0800

    Add abstract MenuItemRegistrar

---
+++ Packages/com.varneon.vudon.menus/Runtime/Scripts/MenuItemRegistrar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Varneon.VUdon.Menus.Abstract
+{
+    public abstract class MenuItemRegistrar : MonoBehaviour
+    {
+        /// <summary>
+        /// Menu where the items will be registered
+        /// </summary>
+        public abstract MenuProvider Menu { get; }
+
+        /// <summary>
+        /// Menu items to be registered on build
+        /// </summary>
+        public abstract MenuItemInfo[] MenuItems { get; }
+    }
+}
