8522c3d531419a65a9095819249b5ae4ccc8440c
Varneon <me@varneon.com>
2023-01-23 20:30:53 -0800

    Add abstract MenuItemInfo

---
+++ Packages/com.varneon.vudon.menus/Runtime/Scripts/MenuItemInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Varneon.VUdon.Menus.Enums;
+
+namespace Varneon.VUdon.Menus.Abstract
+{
+    public abstract class MenuItemInfo : IComparable<MenuItemInfo>
+    {
+        public string Path;
+
+        public MenuEventCallbackReceiver CallbackReceiver;
+
+        public string Tooltip;
+
+        public int Priority = 0;
+
+        public abstract MenuItemType Type { get; }
+
+        public int CompareTo(MenuItemInfo other)
+        {
+            if (other == null) { return 1; }
+
+            int delta = Priority.CompareTo(other.Priority);
+
+            return delta == 0 ? -1 : delta;
+        }
+    }
+}
